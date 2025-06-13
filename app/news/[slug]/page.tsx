29235fb4fecca6a636540741545611535e36f393
syohaku <koide@syohaku.com>
2025-06-13 23:38:04 +0900

    ニュースにSSRを適用

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -13,6 +13,8 @@ dk?:string;
 };
 };
 
+export const revalidate = 0;
+
 export default async function Page( {params,searchParams}:Props){
 const data = await getNewsDetail(params.slug,{
 draftKey:searchParams.dk,
