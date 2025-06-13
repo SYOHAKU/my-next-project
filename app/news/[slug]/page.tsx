14dd1bab7c8299978dacc8d7b71ce32b4aa4f390
syohaku <koide@syohaku.com>
2025-06-13 23:42:27 +0900

    ニュースにISRを適用

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -13,7 +13,7 @@ dk?:string;
 };
 };
 
-export const revalidate = 0;
+export const revalidate = 60;
 
 export default async function Page( {params,searchParams}:Props){
 const data = await getNewsDetail(params.slug,{
