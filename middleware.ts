e320ecfa042d2f47d30e85645d897293e60f1b85
syohaku <koide@syohaku.com>
2025-06-20 22:38:25 +0900

    Basic認証を追加

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+// import { NextRequest, NextResponse } from "next/server";
+
+// export function middleware(request:NextRequest){
+// console.log("middleware:" + request.url);
+
+// return NextResponse.next();
+// }
+
+// export const config = {}
+// matcher: "/about/:path*";
+
+import {createNextAuthMiddleware} from "nextjs-basic-auth-middleware";
+
+export const middleware = createNextAuthMiddleware();
+
+export const config = {
+mathcer: ["/(.*)"],
+}
